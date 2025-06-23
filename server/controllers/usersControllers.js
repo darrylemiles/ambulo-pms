7fde147dbdffbd890c28f54671e2e3895b244475
rrreigun <81731541+rrreigun@users.noreply.github.com>
2025-06-23 19:37:25 +0800

    feat: setup users module API

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -11,6 +11,51 @@ const createUser = expressAsync(async (req, res) => {
     }
 });
 
+const getUsers = expressAsync(async (req, res) => {
+    try {
+        const response = await usersServices.getUsers(req.query);
+        res.json(response);
+    } catch (error) {
+        console.error("Error getting users:", error);
+        throw new Error(error.message || "Failed to get users");
+    }
+});
+
+const getSingleUserById = expressAsync(async (req, res) => {
+    try {
+        const response = await usersServices.getSingleUserById(req.params.userId);
+        res.json(response);
+    } catch (error) {
+        console.error("Error getting user:", error);
+        throw new Error(error.message || "Failed to get user");
+    }
+});
+
+const updateSingleUserById = expressAsync(async (req, res) => {
+    try {
+        const response = await usersServices.updateSingleUserById(req.params.userId, req.body);
+        res.json(response);
+    } catch (error) {
+        console.error("Error updating user:", error);
+        throw new Error(error.message || "Failed to update user");
+    }
+});
+
+const deleteUserById = expressAsync(async (req, res) => {
+    try {
+        const response = await usersServices.deleteUserById(req.params.userId);
+        res.json(response);
+    } catch (error) {
+        console.error("Error deleting user:", error);
+        throw new Error(error.message || "Failed to delete user");
+    }
+});
+
+
 export {
-    createUser
+    createUser,
+    getUsers,
+    getSingleUserById,
+    updateSingleUserById,
+    deleteUserById
 }
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { createUser } from '../controllers/usersControllers.js';
+import { createUser, getUsers, getSingleUserById, updateSingleUserById, deleteUserById } from '../controllers/usersControllers.js';
 
 const router = express.Router();
 
 router.post('/create-user', createUser);
+router.get('/', getUsers);
+router.get('/:userId', getSingleUserById);
+router.patch('/:userId', updateSingleUserById);
+router.delete('/:userId', deleteUserById)
 
 export default router;
