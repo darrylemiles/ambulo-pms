520dde8097f91f4d16243d333580dfe9173ad6f7
Rea Buena <81731541+reiigun@users.noreply.github.com>
2025-08-25 17:28:23 +0800

    refactor: table creation logic

--- server/tables/propertiesPicturesTable.js
+++ server/tables/propertiesPicturesTable.js
@@ -2,6 +2,7 @@ const propertiesPicturesTable = `CREATE TABLE IF NOT EXISTS properties_pictures 
 	image_id INT PRIMARY KEY AUTO_INCREMENT,
 	property_id VARCHAR(255),
 	image_url VARCHAR(255),
+    image_desc VARCHAR(255),
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (property_id) REFERENCES properties(property_id) ON DELETE CASCADE
