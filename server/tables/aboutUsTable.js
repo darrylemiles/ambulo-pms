ad2702d2dff77ef42906bc569db84b981f5300db
Rea Buena <81731541+reiigun@users.noreply.github.com>
2025-09-08 00:33:04 +0800

    feat: auto-create function for About Us table

--- server/tables/aboutUsTable.js
+++ server/tables/aboutUsTable.js
@@ -3,7 +3,13 @@ const aboutUsTable = `CREATE TABLE IF NOT EXISTS about_us (
 	story_content TEXT not null,
 	mission TEXT not null,
 	vision TEXT not null, 
-	core_values TEXT not null	
+	core_values TEXT not null,
+	homepage_about_subtitle TEXT not null,
+	homepage_about_content TEXT not null,
+	about_img1 VARCHAR(255),
+	about_img2 VARCHAR(255),
+	about_img3 VARCHAR(255),
+	about_img4 VARCHAR(255)	
 );`;
 
 export default aboutUsTable;
