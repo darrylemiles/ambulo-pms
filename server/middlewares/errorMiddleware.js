b47c3265df5fb510cec8dd2d7be6428cfe310454
Rea Buena <81731541+reiigun@users.noreply.github.com>
2025-10-19 20:26:17 +0800

    fix: improve error handling in middleware to correctly set response status

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -5,9 +5,12 @@ const notFound = (req, res, next) => {
     };
 
     const errorHandler = (err, req, res, next) => {
-    const error = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(error);
-    res.json({
+    
+    if (res.headersSent) {
+        return next(err);
+    }
+    const status = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
+    res.status(status).json({
         message: err.message,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack
     });
